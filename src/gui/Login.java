861ccd641df4c15dca7379963639bca0781b45d8
Piero Stefano Meza Tejada <i202010805@cibertec.edu.pe>
2021-06-01 01:26:06 -0500

    Finish the LOGIN GUI

--- src/gui/Login.java
+++ src/gui/Login.java
@@ -5,10 +5,29 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+import java.awt.Color;
+import java.awt.Button;
+import java.awt.SystemColor;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JSeparator;
+import java.awt.Font;
+import javax.swing.JPasswordField;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 public class Login extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtUsuario;
+	private JPasswordField txtContraseña;
+	
+	int xx,xy;
+	
 
 	/**
 	 * Launch the application.
@@ -18,6 +37,7 @@ public class Login extends JFrame {
 			public void run() {
 				try {
 					Login frame = new Login();
+					frame.setUndecorated(true);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -30,12 +50,91 @@ public class Login extends JFrame {
 	 * Create the frame.
 	 */
 	public Login() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage("D:\\Steff\\Proyectos\\System\\img\\9ea98f4bcb9bcc6cf6e223478c08f2ca98129d0f.ico"));
+		setBackground(Color.WHITE);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 324, 530);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.setBackground(Color.WHITE);
+		contentPane.setBorder(new EmptyBorder(13, 5, 5, 5));
 		setContentPane(contentPane);
-	}
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, -12, 434, 285);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel_3 = new JLabel("X");
+		lblNewLabel_3.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.exit(0);
+			}
+		});
+		lblNewLabel_3.setFont(new Font("Microsoft New Tai Lue", Font.BOLD, 18));
+		lblNewLabel_3.setBounds(295, 11, 12, 27);
+		panel.add(lblNewLabel_3);
+		lblNewLabel_3.setForeground(Color.RED);
+		lblNewLabel_3.setBackground(Color.RED);
+		
+		JLabel lblNewLabel_2 = new JLabel("");
+	
+		lblNewLabel_2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				
+				xx = e.getX();
+				
+				xy = e.getY();
+				
+			}
+		});
+		lblNewLabel_2.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				int x = e.getXOnScreen();
+				int y = e.getYOnScreen();
+				Login.this.setLocation(x-xx, y-xy);
+			}
+		});
 
+		lblNewLabel_2.setBounds(-55, 11, 420, 275);
+		lblNewLabel_2.setIcon(new ImageIcon("D:\\Steff\\Proyectos\\System\\img\\bg.jpg.png"));
+		panel.add(lblNewLabel_2);
+		
+		Button btnIngresar = new Button("Ingresar");
+		btnIngresar.setForeground(Color.WHITE);
+		btnIngresar.setBackground(new Color(241,57,83) );
+		btnIngresar.setBounds(10, 440, 288, 30);
+		contentPane.add(btnIngresar);
+		
+		txtUsuario = new JTextField();
+		txtUsuario.setBounds(43, 322, 212, 30);
+		contentPane.add(txtUsuario);
+		txtUsuario.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Usuario");
+		lblNewLabel.setFont(new Font("Verdana", Font.PLAIN, 12));
+		lblNewLabel.setBounds(46, 297, 86, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Contrase\u00F1a");
+		lblNewLabel_1.setFont(new Font("Verdana", Font.PLAIN, 12));
+		lblNewLabel_1.setBounds(43, 363, 101, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBackground(Color.GRAY);
+		separator.setBounds(257, 284, -211, 14);
+		contentPane.add(separator);
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setBounds(270, 373, -245, -7);
+		contentPane.add(separator_1);
+		
+		txtContraseña = new JPasswordField();
+		txtContraseña.setBounds(43, 388, 212, 30);
+		contentPane.add(txtContraseña);
+	}
 }
